6f84e80d672d776a1fbbccfda45d888daaa5cea8
Antonyeaster <>
2023-06-23 07:45:00 +0000

    Add dom loading priority to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// Wait until DOM is completly loaded before running game
+// Get button elements and add event listeners
+
+document.addEventListener(DOMContentLoaded, function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (button of buttons) {
+        button.addEventListener("click", function() {
+        let playerChoice = this.getAttribue(data-type);
+        runGame(playerChoice);
+    })
+  }
+}
+
+
+
 function runGame() {
 
 }

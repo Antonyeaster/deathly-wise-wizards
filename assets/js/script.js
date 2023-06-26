12ef6482bac0d01246da019a97c4b7c597606091
Antonyeaster <>
2023-06-26 06:40:47 +0000

    Add favicon to tab bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 let choices = ['resurrection stone', 'elder wand', 'invisibility cloak'];
 let playerImage = document.getElementById('user-side-image');
 let computerImage = document.getElementById('computer-side-image');
-let playerScore = document.getElementById('player-score');
-let computerScore = document.getElementById('computer-score');
 let result = document.getElementById('decision');
 let buttons = document.getElementsByClassName('selected');
 
 for (let button of buttons) {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         let playerChoice = this.getAttribute('data-type');
         runGame(playerChoice);
     })
@@ -19,17 +17,17 @@ for (let button of buttons) {
  */
 
 function runGame(playerChoice) {
-    playerImage = document.querySelector("#user-image");      
-    playerImage.src = `assets/images/${choices[playerChoice].replaceAll(' ','-')}.webp`;
+    playerImage = document.querySelector("#user-image");
+    playerImage.src = `assets/images/${choices[playerChoice].replaceAll(' ', '-')}.webp`;
     playerImage.alt = choices[playerChoice]
-    
+
     let computerChoice = Math.floor(Math.random() * 3)
     computerImage = document.querySelector("#computer-image");
-    computerImage.src = `assets/images/${choices[computerChoice].replaceAll(' ','-')}.webp`;
+    computerImage.src = `assets/images/${choices[computerChoice].replaceAll(' ', '-')}.webp`;
     computerChoice.alt = choices[computerChoice];
 
     let result = checkWinner(choices[playerChoice], choices[computerChoice]);
-    
+
     updateScores(result)
 }
 
@@ -38,15 +36,15 @@ function updateScores(result) {
         incrementUserScore();
     } else if (result === 'computer') {
         incrementComputerScore();
-    }  
-    
-};
+    }
+
+}
 
 /**
  * To check who is the winner with the possible outcomes listed individually 
  */
 function checkWinner(playerChoice, computerChoice) {
-    if (playerChoice === 'resurrection stone' && computerChoice === 'elder wand'){
+    if (playerChoice === 'resurrection stone' && computerChoice === 'elder wand') {
         result.innerHTML = 'WOW, you beat the Dark Lord';
         return 'user'
     } else if (playerChoice === 'elder wand' && computerChoice === 'resurrection stone') {
@@ -55,7 +53,7 @@ function checkWinner(playerChoice, computerChoice) {
     } else if (playerChoice === 'resurrection stone' && computerChoice === 'invisibility cloak') {
         result.innerHTML = 'The Dark Lord is wise! Try harder next time!'
         return 'computer'
-    }  else if (playerChoice === 'invisibility cloak' && computerChoice === 'resurrection stone') {
+    } else if (playerChoice === 'invisibility cloak' && computerChoice === 'resurrection stone') {
         result.innerHTML = "That's totally barbaric! Well done, You win."
         return 'user'
     } else if (playerChoice === 'elder wand' && computerChoice === 'invisibility cloak') {
@@ -64,8 +62,8 @@ function checkWinner(playerChoice, computerChoice) {
     } else if (playerChoice === 'invisibility cloak' && computerChoice === 'elder wand') {
         result.innerHTML = "The Dark Lord rises again! Try again"
         return 'computer'
-    } else (playerChoice === computerChoice) 
-        result.innerHTML = "I guess a Draw is ok..... FOR A MUGGLE! Let's beat the Dark lord"
+    } else (playerChoice === computerChoice)
+    result.innerHTML = "I guess a Draw is ok..... FOR A MUGGLE! Let's beat the Dark lord"
 }
 
 /**
@@ -74,7 +72,7 @@ function checkWinner(playerChoice, computerChoice) {
 
 function incrementUserScore() {
     let previousScore = parseInt(document.getElementById('player-score').innerText);
-    document.getElementById('player-score').innerText = ++previousScore; 
+    document.getElementById('player-score').innerText = ++previousScore;
 }
 
 /**

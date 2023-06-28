2a74b1292d581c48e507368d2c7fac2a5cc8d211
Antonyeaster <>
2023-06-28 21:27:36 +0000

    Add modal for game rules pop up, change from alert pop up

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,3 +85,29 @@ function incrementComputerScore() {
 function rulesPopUp() {
     alert("Rules\nElder Wand Destroys Invisibility Cloak\nInvisibility Cloaks Outsmarts Resurrection Stone\nResurrection Stone Eliminates Elder Wand")
 }
+
+// Get the modal
+var modal = document.getElementById("myRulesModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myRulesBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
